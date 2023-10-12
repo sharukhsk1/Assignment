b729d3585bfb682c20df037e5234b3b7c8dcc9b0
sharukhsk1 <sharukhsk1>
2023-10-12 11:21:20 +0530

    Passed

---
+++ Roman_Number.java
@@ -0,0 +1,55 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Roman_Number{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a Roman numeral: ");
+        String romanNumeral = scanner.nextLine().toUpperCase();
+        scanner.close();
+
+        int result = romanToInteger(romanNumeral);
+        if (result != -1) {
+            System.out.println("Integer equivalent: " + result);
+        } else {
+            System.out.println("Invalid Roman numeral.");
+        }
+    }
+
+    public static int romanToInteger(String s) {
+        Map<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+        int prevValue = 0;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int value = romanMap.getOrDefault(c, -1);
+
+            if (value == -1) {
+                return -1; // Invalid Roman numeral
+            }
+
+            if (value < prevValue) {
+                result -= value;
+            } else {
+                result += value;
+            }
+
+            prevValue = value;
+        }
+
+        return result;
+    }
+}
+
+    
+
