f2639f613e825f13355389d81d7e1f777989b336
sharukhsk1 <sharukhsk1>
2023-10-12 11:37:48 +0530

    passed

--- Roman_Number.java
+++ Roman_Number.java
@@ -1,8 +1,6 @@
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Scanner;
 
-public class Roman_Number{
+public class Roman_Number {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter a Roman numeral: ");
@@ -10,46 +8,43 @@ public class Roman_Number{
         scanner.close();
 
         int result = romanToInteger(romanNumeral);
-        if (result != -1) {
-            System.out.println("Integer equivalent: " + result);
-        } else {
-            System.out.println("Invalid Roman numeral.");
-        }
+        System.out.println("Integer equivalent: " + result);
     }
 
     public static int romanToInteger(String s) {
-        Map<Character, Integer> romanMap = new HashMap<>();
-        romanMap.put('I', 1);
-        romanMap.put('V', 5);
-        romanMap.put('X', 10);
-        romanMap.put('L', 50);
-        romanMap.put('C', 100);
-        romanMap.put('D', 500);
-        romanMap.put('M', 1000);
-
         int result = 0;
-        int prevValue = 0;
 
-        for (int i = s.length() - 1; i >= 0; i--) {
-            char c = s.charAt(i);
-            int value = romanMap.getOrDefault(c, -1);
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = s.charAt(i);
+            int currentValue = charToValue(currentChar);
 
-            if (value == -1) {
-                return -1; // Invalid Roman numeral
-            }
+            if (i < s.length() - 1) {
+                char nextChar = s.charAt(i + 1);
+                int nextValue = charToValue(nextChar);
 
-            if (value < prevValue) {
-                result -= value;
+                if (currentValue < nextValue) {
+                    result -= currentValue;
+                } else {
+                    result += currentValue;
+                }
             } else {
-                result += value;
+                result += currentValue;
             }
-
-            prevValue = value;
         }
 
         return result;
     }
+
+    public static int charToValue(char c) {
+        switch (c) {
+            case 'I': return 1;
+            case 'V': return 5;
+            case 'X': return 10;
+            case 'L': return 50;
+            case 'C': return 100;
+            case 'D': return 500;
+            case 'M': return 1000;
+            default: return 0; // Invalid character
+        }
+    }
 }
-
-    
-
